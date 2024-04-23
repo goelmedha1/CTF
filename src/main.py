ad12ba10e0e95126656b6cacb708ace84a9fbdd7
goelmedha1 <124646874+goelmedha1@users.noreply.github.com>
2024-04-22 23:04:00 -0400

    Added static functionality

--- src/main.py
+++ src/main.py
@@ -1,12 +1,24 @@
+# from flask import Flask, render_template
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def index():
+#     return render_template('webpage.html')
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
 from flask import Flask, render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
     return render_template('webpage.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
